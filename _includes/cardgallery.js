991221e55c6a3504c73553755df06c1da31379c6
Adamu Elisha Inuwa <40290364+elishadamu97@users.noreply.github.com>
2024-09-13 14:43:22 +0100

    solve card gallery issue

--- _includes/cardgallery.js
+++ _includes/cardgallery.js
@@ -2,7 +2,7 @@ const lightbox = document.createElement('div');
   lightbox.id = 'lightbox';
   document.body.appendChild(lightbox);
 
-  const images = document.getElementById('img');
+  const images = document.querySelectorAll('img');
   let currentIndex = 0;
 
   const updateLightbox = (index) => {
